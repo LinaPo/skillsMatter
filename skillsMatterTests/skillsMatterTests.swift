593c440f750e6576db6788c26e573705386cad9f
Vilnius Swedbank <vilnius@Vilniuss-MacBook-Pro.local>
2017-06-22 10:34:46 +0300

    Initial Commit

---
+++ skillsMatterTests/skillsMatterTests.swift
@@ -0,0 +1,36 @@
+//
+//  skillsMatterTests.swift
+//  skillsMatterTests
+//
+//  Created by Vilnius Swedbank on 22/06/2017.
+//  Copyright © 2017 Vilnius Swedbank. All rights reserved.
+//
+
+import XCTest
+@testable import skillsMatter
+
+class skillsMatterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
